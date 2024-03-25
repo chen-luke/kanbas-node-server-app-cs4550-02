b11f34a359cb63eea5cd0b4d8a3ffe29b664ca10
chen-luke <72319988+chen-luke@users.noreply.github.com>
2024-03-25 00:07:57 -0400

    added crud array routes

--- Lab5.js
+++ Lab5.js
@@ -12,8 +12,71 @@ const module = {
   completed: false
 };
 
+const todos = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+  { id: 3, title: "Task 3", completed: false },
+  { id: 4, title: "Task 4", completed: true },
+];
+
 
 const Lab5 = (app) => {
+
+  app.get("/a5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+
+
+  app.get("/a5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    const todoIndex = todos.indexOf(todo);
+    if (todoIndex !== -1) {
+      todos.splice(todoIndex, 1);
+    }
+    res.json(todos);
+  });
+
+
+  app.get("/a5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+
+
+  app.get("/a5/todos", (req, res) => {
+    const { completed } = req.query;
+    if (completed !== undefined) {
+      const completedBool = completed === "true";
+      const completedTodos = todos.filter(
+        (t) => t.completed === completedBool);
+      res.json(completedTodos);
+      return;
+    }
+    res.json(todos);
+  });
+
+
+
+  app.get("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    res.json(todo);
+  });
+
+
+  app.get("/a5/todos", (req, res) => {
+    res.json(todos);
+  });
+
 
   app.get("/a5/module", (req, res) => {
     res.json(module);
@@ -24,19 +87,19 @@ const Lab5 = (app) => {
   })
 
   app.get("/a5/module/name/:newName", (req, res) => {
-    const {newName} = req.params;
+    const { newName } = req.params;
     module.name = newName;
     res.json(module);
   })
 
   app.get("/a5/assignment/score/:newScore", (req, res) => {
-    const {newScore} = req.params;
+    const { newScore } = req.params;
     assignment.score = parseInt(newScore);
     res.json(assignment);
   })
 
   app.get("/a5/assignment/completed/:completed", (req, res) => {
-    const {completed} = req.params;
+    const { completed } = req.params;
     assignment.completed = completed;
     res.json(assignment);
   })
