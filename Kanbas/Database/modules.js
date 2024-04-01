c523db9dd0543f77eaea1599609bd3f88a43f1b1
chen-luke <72319988+chen-luke@users.noreply.github.com>
2024-03-31 20:18:34 -0400

    moved database files to backend, assignments, courses. grades...

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,171 @@
+export default [
+    {
+      "_id": "M100",
+      "name": "OOD Week 1",
+      "description": "Basic principles of rocket propulsion and rocket engines.",
+      "course": "RS3500",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "OOD 2",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "OOD 3",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "OOD 4",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+      {
+        "_id": "M101",
+        "name": "Introduction to Rocket Propulsion",
+        "description": "Basic principles of rocket propulsion and rocket engines.",
+        "course": "RS4530",
+        "lessons": [
+          {
+            "_id": "L101",
+            "name": "History of Rocketry",
+            "description": "A brief history of rocketry and space exploration.",
+            "module": "M101"
+          },
+          {
+            "_id": "L102",
+            "name": "Rocket Propulsion Fundamentals",
+            "description": "Basic principles of rocket propulsion.",
+            "module": "M101"
+          },
+          {
+            "_id": "L103",
+            "name": "Rocket Engine Types",
+            "description": "Overview of different types of rocket engines.",
+            "module": "M101"
+          }
+        ]
+      },
+      {
+        "_id": "M102",
+        "name": "Env Week 0",
+        "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+        "course": "RS2800",
+        "lessons": [
+          {
+            "_id": "L201",
+            "name": "Env Week 1",
+            "description": "Env Week 1",
+            "module": "M102"
+          },
+          {
+            "_id": "L202",
+            "name": "Env Week 2",
+            "description": "Env Week 2",
+            "module": "M102"
+          },
+          {
+            "_id": "L203",
+            "name": "Env Week 3",
+            "description": "Env Week 3",
+            "module": "M102"
+          }
+        ]
+      },
+      {
+        "_id": "M103",
+        "name": "CS Week 1",
+        "description": "CS Module 1",
+        "course": "RS1234",
+        "lessons": [
+          {
+            "_id": "L301",
+            "name": "CS Module 1.1",
+            "description": "CS Module 2",
+            "module": "M103"
+          },
+          {
+            "_id": "L302",
+            "name": "CS Module 1.2",
+            "description": "CS Module 1.2",
+            "module": "M103"
+          }
+        ]
+      },
+      {
+        "_id": "M201",
+        "name": "Fundamentals of Math",
+        "description": "Basic math concept and dynamics principles.",
+        "course": "RS1800",
+        "lessons": [
+          {
+            "_id": "L301",
+            "name": "Math I",
+            "description": "Overview of Math I",
+            "module": "M103"
+          },
+          {
+            "_id": "L302",
+            "name": "Math II",
+            "description": "Overview of Math II",
+            "module": "M103"
+          },
+          {
+            "_id": "L303",
+            "name": "Math III",
+            "description": "Overview of Math III",
+            "module": "M103"
+          }
+        ]
+      },
+      {
+        "_id": "M202",
+        "name": "Subsonic and Supersonic Flow",
+        "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+        "course": "RS102"
+      },
+      {
+        "_id": "M203",
+        "name": "Aerodynamic Heating",
+        "description": "Study of aerodynamic heating and thermal protection systems.",
+        "course": "RS102"
+      },
+      {
+        "_id": "M301",
+        "name": "Spacecraft Structural Design",
+        "description": "Fundamentals of designing spacecraft structures and materials selection.",
+        "course": "RS103"
+      },
+      {
+        "_id": "M302",
+        "name": "Week 1",
+        "description": "Understanding orbital dynamics and mission planning.",
+        "course": "RS3540",
+        "lessons": [
+          {
+            "_id": "L301",
+            "name": "Database Design I",
+            "description": "Overview of different types of rocket nozzles.",
+            "module": "M103"
+          }]
+      },
+      {
+        "_id": "M302",
+        "name": "Week 2",
+        "description": "Understanding orbital dynamics and mission planning.",
+        "course": "RS3540",
+        "lessons": [
+          {
+            "_id": "L301",
+            "name": "Database Design II",
+            "description": "Overview of different types of rocket nozzles.",
+            "module": "M103"
+          }]
+      }
+    ]
+    
